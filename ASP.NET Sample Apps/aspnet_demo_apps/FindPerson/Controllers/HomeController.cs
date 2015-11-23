2c8b2adc07381ad73ac6562edd5bc7f7a0ed730b
Anthony Stevens <anthonys@crowdify.com>
2015-11-23 11:30:17 -0800

    Added FindPerson ASP.NET sample app (#A1)

---
+++ ASP.NET Sample Apps/aspnet_demo_apps/FindPerson/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+using FindPerson.Models;
+using ProApiLibrary.Api.Clients;
+using ProApiLibrary.Api.Exceptions;
+using ProApiLibrary.Api.Queries;
+using ProApiLibrary.Api.Responses;
+using ProApiLibrary.Data.Entities;
+
+namespace FindPerson.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		[HttpPost]
+	    public ActionResult Index(SearchModel model)
+	    {
+		    var searchResult = this.SearchProApi(model);
+		    return View("~/Views/Home/Index.cshtml", searchResult);
+	    }
+
+		public Response<IPerson> SearchProApi(SearchModel model)
+		{
+			var apiKey = ConfigurationManager.AppSettings["api_key"];
+			var client = new Client(apiKey);
+			var query = new PersonQuery(model.FirstName, null, model.LastName, model.City, model.State, model.PostalCode);
+			Response<IPerson> response;
+			try
+			{
+				response = client.FindPeople(query);
+			}
+			catch (FindException)
+			{
+				throw new Exception(String.Format("FindPerson lookup for {0} {1} failed!", model.FirstName, model.LastName));
+			}
+
+			return response;
+
+
+		}
+    }
+}
---
+++ ASP.NET Sample Apps/aspnet_demo_apps/FindPerson/Models/SearchModel.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FindPerson.Models
+{
+	public class SearchModel
+	{
+		private string _name;
+		private string _address;
+		private string _where;
+		private string _city;
+		private string _state;
+		private string _zip;
+
+		public string LastName 
+		{ 
+			get
+			{
+				if (_name.Contains(" "))
+				{
+					return _name.Substring(_name.IndexOf(" ", StringComparison.InvariantCultureIgnoreCase) + 1);
+				}
+				return _name;
+			}
+		}
+
+		public string FirstName
+		{
+			get
+			{
+				if (_name.Contains(" "))
+				{
+					return _name.Substring(0, _name.IndexOf(" ", StringComparison.InvariantCultureIgnoreCase));
+				}
+				return _name;
+			}
+		}
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		public string Address
+		{
+			get { return _address; }
+			set { _address = value; }
+		}
+
+		public string Where
+		{
+			get { return _where; }
+			set
+			{
+				_where = value;
+				var city = _where;
+				var state = _where;
+				var zip = _where;
+				var pat = "^\\d.*";
+				if (Regex.IsMatch(_where, pat))
+				{
+					_zip = _where.Trim();
+					state = "";
+					city = "";
+				}
+				else
+				{
+					// do we have a state?
+					pat = ".*\\s*(?<state>[A-Za-z]{2})\\s*.*";
+					if (Regex.IsMatch(_where, pat))
+					{
+						var regex = new Regex(pat);
+						var match = regex.Match(_where);
+						state = match.Groups["state"].Value;
+						var index = _where.IndexOf(state, StringComparison.InvariantCultureIgnoreCase);
+						city = _where.Substring(0, index - 1).Trim();
+						zip = _where.Substring(index + state.Length + 1).Trim();
+					}
+					else
+					{
+						// no state
+						// is there a numeric zip code at the end?
+						pat = ".*(?<zip>\\d{5})\\s*$";
+						if (Regex.IsMatch(_where, pat))
+						{
+							var regex = new Regex(pat);
+							var match = regex.Match(_where);
+							zip = match.Groups["zip"].Value;
+							var idx = _where.IndexOf(zip, StringComparison.InvariantCultureIgnoreCase);
+							city = _where.Substring(0, idx - 1);
+							state = "";
+						}
+						else
+						{
+							// no state, no zip: all is city
+							city = _where;
+							state = "";
+							zip = "";
+						}
+					}
+
+				}
+				_city = city;
+				_state = state;
+				_zip = zip;
+			}
+		}
+
+		public string City
+		{
+			get { return _city; }
+		}
+
+		public string State
+		{
+			get { return _state; }
+		}
+
+		public string PostalCode
+		{
+			get { return _zip; }
+		}
+
+	}
+}
