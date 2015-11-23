f2e6b08dca652666b032c3825f4d20a1907cace7
Anthony Stevens <anthonys@crowdify.com>
2015-11-23 12:53:52 -0800

    Added ASP.NET sample app A5: Call Center

---
+++ ASP.NET Sample Apps/aspnet_demo_apps/CallCenter/Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CallCenter.Classes;
+using ProApiLibrary.Api.Clients;
+using ProApiLibrary.Api.Exceptions;
+using ProApiLibrary.Api.Queries;
+using ProApiLibrary.Api.Responses;
+using ProApiLibrary.Data.Entities;
+
+namespace CallCenter.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new List<PhoneData>());
+        }
+
+	    [HttpPost]
+	    public ActionResult Index(string phoneNumber)
+	    {
+		    var callList = Session["Calls"] as List<PhoneData>;
+		    if (callList == null)
+		    {
+			    callList = new List<PhoneData>();
+		    }
+		    var call = this.ConstructPhoneData(phoneNumber);
+		    callList.Add(call);
+		    return View(callList);
+	    }
+
+	    protected internal virtual PhoneData ConstructPhoneData(string phoneNumber)
+	    {
+		    var response = this.SearchProApi(phoneNumber);
+		    var notes = new List<string>();
+		    var lineType = LineType.Other;
+		    var doNotCall = false;
+			string carrier = null;
+		    int? reputationLevel = null;
+		    var isPrepaid = false;
+
+		    if (!response.IsFailure)
+		    {
+			    var phone = response.Results.FirstOrDefault();
+			    if (phone != null)
+			    {
+				    lineType = phone.LineType.GetValueOrDefault(LineType.Other);
+				    doNotCall = phone.DoNotCall.GetValueOrDefault(false);
+				    carrier = phone.Carrier;
+				    reputationLevel = phone.Reputation == null ? (int?) null : phone.Reputation.Level;
+				    isPrepaid = phone.IsPrepaid ?? false;
+			    }
+		    }
+
+		    if (lineType != LineType.Other)
+		    {
+			    notes.Add(lineType.ToString());
+		    }
+		    if (doNotCall)
+		    {
+			    notes.Add("Do Not Call Registered");
+		    }
+		    if (!string.IsNullOrWhiteSpace(carrier))
+		    {
+			    notes.Add(carrier);
+		    }
+			var call = new PhoneData()
+			{
+				CallTime = DateTime.Now,
+				PhoneNumber = phoneNumber,
+				PrioritizationNotes = notes,
+				LineType = lineType,
+				DoNotCall = doNotCall,
+				Carrier = carrier,
+				ReputationLevel = reputationLevel,
+				IsPrepaid = isPrepaid,
+			};
+
+		    return call;
+	    }
+
+	    protected internal virtual Response<IPhone> SearchProApi(string phoneNumber)
+	    {
+		    Response<IPhone> response;
+
+		    var apiKey = ConfigurationManager.AppSettings["api_key"];
+		    var client = new Client(apiKey);
+		    var query = new PhoneQuery(phoneNumber);
+		    try
+		    {
+			    response = client.FindPhones(query);
+		    }
+		    catch (FindException)
+		    {
+			    throw new Exception(string.Format("ReversePhone lookup for {0} failed!", phoneNumber));
+		    }
+
+		    return response;
+	    }
+    }
+}
---
+++ ASP.NET Sample Apps/aspnet_demo_apps/CallCenter/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using CallCenter.Classes;
+
+namespace CallCenter
+{
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		}
+
+		protected void Session_Start()
+		{
+			Session.Add("Calls", new List<PhoneData>());
+		}
+	}
+}
